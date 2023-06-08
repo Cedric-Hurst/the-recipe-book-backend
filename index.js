a60f012e3ac07471b3d7e1f3ffb3f1c85f994ffa
Cedric-Hurst <cjhurstftw@yahoo.com>
2023-06-08 17:22:59 -0400

    add Get recipes

--- index.js
+++ index.js
@@ -14,12 +14,74 @@ const db = mysql.createConnection({
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-	res.send('working');
+app.get('/recipes', (req, res) => {
+	let countRes = 0;
+	db.query('select count(*) as total from recipes', (err, results) => {
+		if (err) throw err;
+		countRes = results[0].total;
+	});
+	const instructions = [];
+	db.query('select * from instructions', (err, results) => {
+		if (err) throw err;
+		for (let i = 0; i < countRes; i++) {
+			instructions.push([]);
+		}
+		results.map((instrObj) =>
+			instructions[instrObj.recipe_id - 1].push(instrObj.instruction)
+		);
+	});
+	const ingredients = [];
+	db.query('select * from ingredients', (err, results) => {
+		if (err) throw err;
+		for (let i = 0; i < countRes; i++) {
+			ingredients.push([]);
+		}
+		results.map((ingreObj) =>
+			ingredients[ingreObj.recipe_id - 1].push({
+				ingredient: ingreObj.ingredient,
+				qty: ingreObj.qty,
+				measure: ingreObj.measure,
+				description: ingreObj.description,
+			})
+		);
+	});
+	const categories = [];
+	db.query('select * from fullCat', (err, results) => {
+		if (err) throw err;
+		for (let i = 0; i < countRes; i++) {
+			categories.push([]);
+		}
+		results.map((catObj) =>
+			categories[catObj.recipe_id - 1].push(catObj.catName)
+		);
+	});
+	const recipes = [];
+	db.query('select * from recipes_users', (err, results) => {
+		if (err) throw err;
+		for (let i = 0; i < countRes; i++) {
+			recipes.push({
+				recipeTitle: results[i].recipeTitle,
+				id: results[i].id,
+				favorite: false,
+				servings: results[i].serving,
+				img: results[i].img,
+				category: [...categories[i]],
+				timing: {
+					prepHr: results[i].prepHr,
+					prepMin: results[i].prepMin,
+					cookHr: results[i].cookHr,
+					cookMin: results[i].cookMin,
+				},
+				ingredients: [...ingredients[i]],
+				instructions: [...instructions[i]],
+				author: results[i].author,
+			});
+		}
+		return res.json(recipes);
+	});
 });
-
 app.get('/test', (req, res) => {
-	db.query('select * from categories', (err, results) => {
+	db.query('select * from recipes', (err, results) => {
 		if (err) throw err;
 		return res.json(results);
 	});
